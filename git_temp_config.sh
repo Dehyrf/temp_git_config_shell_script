1f333d6cb88182967598857861b12ecd9e0bd3d6
Dehyrf <Dehyrf@users.noreply.github.com>
2015-12-16 12:03:53 -0600

    Minor fix

--- git_temp_config.sh
+++ git_temp_config.sh
@@ -11,5 +11,6 @@ touch ~/.bashrc
 echo "alias gitchange='git config user.name "$USERNAME"
 git config user.email "$EMAIL"'" >> ~/.bashrc
 
-echo Please log out and log back in, then use 'gitchange' within a repo to change the user to the one you set
+echo Please use 'gitchange' within a repo to change the user to the one you set!
 echo
+source ~/.bashrc
